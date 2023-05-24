75d020d9951e6274de6411220545c10f709c7553
Alberto Urraro <81329291+cyberTechA@users.noreply.github.com>
2023-05-24 11:30:16 +0200

    funzione python  bar plot per gli intervalli CI

---
+++ 1_Benchmark/grafico_bar_plot.py
@@ -0,0 +1,58 @@
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def customized_barplot(start_value, max_value_graf, end_value, position):
+    # Calcola la lunghezza del bar plot
+    length = end_value - start_value
+    
+    # Prepara i dati per il grafico a barre
+    x = np.array([position])  # Array degli indici
+    y = np.array([length])  # Array dei valori
+    
+    # Crea il grafico a barre con offset in altezza
+    plt.bar(x, y, color='blue', bottom=start_value)
+    
+    # Imposta l'asse Y da zero al massimo valore
+    plt.ylim(155000, max_value_graf)
+    
+    # Imposta l'asse X per avere solo valori positivi
+    plt.xlim(0, position + 1)
+    
+    # Mostra gli assi X e Y
+    plt.axis('on')
+    
+   
+    
+#massimo valore asse y
+max_value_graf = 175000
+
+
+# Valori per la prima macchina
+start_value_1 = 160112.5
+end_value_1 = 160475.9
+position_1 = 1
+
+# Valori per la seconda macchina
+start_value_2 = 163443
+end_value_2 = 164231.7
+position_2 = 2
+
+# Crea il primo bar plot
+customized_barplot(start_value_1, max_value_graf, end_value_1,position_1)
+
+# Crea il secondo bar plot
+customized_barplot(start_value_2, max_value_graf, end_value_2, position_2)
+
+# Imposta la posizione dei tick sull'asse X
+plt.xticks([position_1, position_2], ['Macchina A', 'Macchina B'])
+plt.title('1000000')  # Titolo del grafico
+# Mostra entrambi i bar plot
+plt.show()
+
+
+
+
+
+
