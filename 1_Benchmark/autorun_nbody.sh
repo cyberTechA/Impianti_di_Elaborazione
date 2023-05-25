c45c15cc6e8e350e341fdb55d777c3ec1eddc2ec
Alberto <al.urraro@studenti.unina.it>
2023-05-25 12:31:47 +0200

    prove extra

--- 1_Benchmark/autorun_nbody.sh
+++ 1_Benchmark/autorun_nbody.sh
@@ -2,11 +2,11 @@
 
 # Number of times to restart WSL
 num_restarts=1
-save_path="/Users/raro69/Desktop/Impianti Alberto e Giuseppe/1_Benchmark/M1Pro_A/risultati1000000.txt"
+save_path="/Users/raro69/Desktop/Impianti Alberto e Giuseppe/1_Benchmark/M1Pro_A/risultati_r_1_1000000.txt"
 
 cd nbody/
 
 for i in $(seq 1 $num_restarts); do
   # Run program
-  bash ./launch_nbody.sh -r 5 -n 1000000 >> "$save_path"
+  bash ./launch_nbody.sh -r 1 -n 1000000 >> "$save_path"
 done
